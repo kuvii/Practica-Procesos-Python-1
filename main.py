febb22af5099671932527ee9f4b687bf97e958b4
kuvii <kuvilet@gmail.com>
2022-11-09 13:51:21 +0000

    Feat(main): Add Process Creator on Windows

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-import os
+import os, platform
+from multiprocessing import Process
 from time import sleep
 
-def padre():
+if (platform.system() == "Windows"):
+    def hijo():
+        print("  > Son pid: ", os.getpid())
+        sleep(5)
+        print("Process ", os.getpid(), " ended")
+        os._exit(0)
+    
+    def padre():
+        print("Pid Windows, padre: ", os.getpid())
+        for i in range(processRepeat) :
+            p = Process(target=hijo)
+            p.start()
+            p.join(0)
+    
+    if __name__ == '__main__' :
+        processRepeat = int(input("Cuantos procesos quieres abrir: "))
+        padre()
+
+elif (platform.system() == "Linux"):
+    
+    def padre():
         print("Pid padre: ", os.getpid())
         for i in range(processRepeat):
             sonPid = os.fork()
@@ -9,12 +30,12 @@ def padre():
                 hijo()
           
     
-def hijo():
-    print("  > Son pid: ", os.getpid())
-    sleep(5)
-    print("Process ", os.getpid(), " ended")
-    os._exit(0)
-    
-if __name__ == '__main__' :
-    processRepeat = int(input("Cuantos procesos quieres abrir: "))
-    padre()
+    def hijo():
+        print("  > Son pid: ", os.getpid())
+        sleep(5)
+        print("Process ", os.getpid(), " ended")
+        os._exit(0)
+        
+    if __name__ == '__main__' :
+        processRepeat = int(input("Cuantos procesos quieres abrir: "))
+        padre()
